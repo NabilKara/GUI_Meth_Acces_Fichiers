feb5bdbfd9fd4615f1ab32b3004a34f8005249a6
mimobn <112497685+mimobn@users.noreply.github.com>
2023-12-28 16:37:59 +0000

    Update main.c

--- main.c
+++ main.c
@@ -6,12 +6,7 @@
 int main()
 {
 
-  if (SDL_Init(SDL_INIT_VIDEO)<0){  
-        printf("SDL could not be initialized");
-        SDL_GetError();
-  }else {
-        printf("SDL READY \n");
-
+ 
   }
 
 
