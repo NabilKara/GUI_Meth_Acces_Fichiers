85342138faa02e08ed7b8ff04dc3a8fa14dbc4d2
SIFITarekMazigh <sifitarekmazigh07@gmail.com>
2023-12-28 14:11:14 +0100

    modifier les parametres de la procedure intToStr et ajouter son corps , ajouter la fonction nbPos

--- lib/utils.c
+++ lib/utils.c
@@ -14,4 +14,40 @@ int strToInt(char ch[],int taille)
     return Int;
     
 }
-void intToStr(int n,char ch[]);
+
+int nbPos(int n)
+{
+    if (n > 999)
+    {
+        return -1 ; // faux
+    }
+    
+    int i=0 ;
+    while (n != 0)
+    {
+        i = i + 1 ;
+        n = n / 10 ;
+    }
+
+    return i;
+
+}
+
+void intToStr(int n,char ch[])
+{ 
+    if(nbPos(n) == -1)
+    {
+        printf("ERREUR !!!!");
+        exit(1) ;
+    }
+    else
+    {
+        
+        for (int i = 2; i >= 0; i--) // 2 car le nb max est : 999 
+        {
+            ch[i] = n % 10 + 48 ; 
+            n = n / 10 ;
+        }
+        
+    }
+}
